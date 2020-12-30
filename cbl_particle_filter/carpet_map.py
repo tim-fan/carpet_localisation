4e7439884fe5d7ba2e0b6507573131e20a1662d6
tim-fan <tim.fanselow@gmail.com>
2020-12-30 21:52:45 +1300

    Add random map generation

--- cbl_particle_filter/carpet_map.py
+++ cbl_particle_filter/carpet_map.py
@@ -5,6 +5,7 @@ defines representation of carpet map
 from typing import Tuple
 import numpy as np
 
+
 class CarpetMap:
     """
     Representation of carpet.
@@ -24,8 +25,7 @@ class CarpetMap:
     cell covers the x-range (in m):
     [0.8:1.2)
     """
-
-    def __init__(self, grid:np.array, cell_size:float):
+    def __init__(self, grid: np.array, cell_size: float):
         """
         grid: 2d array of carpet colors (ints)
         cell_size: size (in meters) of carpet grid cells
@@ -33,7 +33,7 @@ class CarpetMap:
         self.grid = grid
         self.cell_size = cell_size
 
-    def get_color_at_coords(self, coords:np.array) -> np.array:
+    def get_color_at_coords(self, coords: np.array) -> np.array:
         """
         lookup color (int) in map at given x,y coordinates (in meters)
         input coords are given as nx2 array (for n coordinates)
@@ -41,18 +41,25 @@ class CarpetMap:
         Input coordinates which lie outside the bounds of the map
         will have an associated output value of -1
         """
-        colors = np.full((coords.shape[0]),-1)
+        colors = np.full((coords.shape[0]), -1)
 
-        x_indices = np.floor(coords[:,0] / self.cell_size).astype(np.int)
-        y_indices = np.ceil(self.grid.shape[0] - 1  - (coords[:,1] / self.cell_size)).astype(np.int)
+        x_indices = np.floor(coords[:, 0] / self.cell_size).astype(np.int)
+        y_indices = np.ceil(self.grid.shape[0] - 1 -
+                            (coords[:, 1] / self.cell_size)).astype(np.int)
 
         # avoid indexing points which are out of bounds
-        in_bounds = (
-            (x_indices >= 0) &
-            (x_indices < self.grid.shape[1]) &
-            (y_indices >= 0) &
-            (y_indices < self.grid.shape[0])
-        )
-        colors[in_bounds] = self.grid[y_indices[in_bounds], x_indices[in_bounds]]
+        in_bounds = ((x_indices >= 0) & (x_indices < self.grid.shape[1]) &
+                     (y_indices >= 0) & (y_indices < self.grid.shape[0]))
+        colors[in_bounds] = self.grid[y_indices[in_bounds],
+                                      x_indices[in_bounds]]
 
         return colors
+
+
+def generate_random_map(shape: Tuple[int, int], cell_size,
+                        n_colors: int) -> CarpetMap:
+    """
+    Generate a random map of the given size with the given number of colors
+    """
+    return CarpetMap(grid=np.random.randint(0, high=n_colors, size=shape),
+                     cell_size=cell_size)
--- cbl_particle_filter/tests/test_map.py
+++ cbl_particle_filter/tests/test_map.py
@@ -1,26 +1,29 @@
-from ..carpet_map import CarpetMap
+from ..carpet_map import CarpetMap, generate_random_map
 import numpy as np
+
 
 def make_test_map() -> CarpetMap:
     return CarpetMap(
         grid=np.array([
-            [1,9,2],
-            [8,3,7],
-            [4,6,5],
+            [1, 9, 2],
+            [8, 3, 7],
+            [4, 6, 5],
         ]),
         cell_size=0.4,
     )
 
+
 def test_init():
     carpet = make_test_map()
     assert isinstance(carpet, CarpetMap)
+
 
 def test_get_color_at_coords():
     carpet = make_test_map()
 
     coords = np.array([
         # two points in the bottom left cell, near the origin:
-        [0.0,0.0], 
+        [0.0, 0.0],
         [0.3, 0.1],
         # a series of randomly chosen points:
         [0.5, 0.7],
@@ -35,7 +38,7 @@ def test_get_color_at_coords():
         [1.21, 0.1],
         [0.1, 1.21],
     ])
-    
+
     expected_colors = np.array([
         4,
         4,
@@ -50,5 +53,23 @@ def test_get_color_at_coords():
         -1,
     ])
 
-    np.testing.assert_array_equal(expected_colors, carpet.get_color_at_coords(coords))
+    np.testing.assert_array_equal(expected_colors,
+                                  carpet.get_color_at_coords(coords))
 
+
+def test_generate_random_map():
+    shape = (10, 20)
+    cell_size = 0.5
+    n_colors = 4
+    carpet = generate_random_map(shape, cell_size, n_colors)
+
+    assert isinstance(carpet, CarpetMap)
+    assert carpet.cell_size == cell_size
+    assert carpet.grid.shape == shape
+    assert len(np.unique(carpet.grid)) == n_colors
+
+    # for debug visualisation set plot=True
+    plot = False
+    if plot:
+        from ..visualisation import plot_map
+        plot_map(carpet)
